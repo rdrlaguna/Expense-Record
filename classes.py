e1cc37fdc6e2a59ceca89f02150d729167720831
rdrlaguna <rdr.laguna@gmail.com>
2024-11-24 08:13:03 +0100

    Create Frame class

--- classes.py
+++ classes.py
@@ -7,4 +7,19 @@ class App(customtkinter.CTk):
 
         self.title("Expense Record")
         self.geometry("800x450")
-        self.grid_columnconfigure(0, weight=1)
+        self.grid_columnconfigure((0, 1, 2), weight=1)
+
+        # Add frame
+        self.frame_1 = Frame(self, name="Add Element", height=400)
+        self.frame_1.grid(row=0, column=0, padx=20, pady=20, sticky="nsew")
+
+
+
+
+class Frame(customtkinter.CTkFrame):
+    def __init__(self, master, name="Add Name", **kwargs):
+        super().__init__(master, **kwargs)
+
+        # Add label to frame
+        self.label = customtkinter.CTkLabel(self, text=name)
+        self.label.grid(row=0, column=0, padx=10)
