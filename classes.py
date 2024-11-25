49fd96a01dee0f0290f7493c38689e5ecd72a16c
rdrlaguna <rdr.laguna@gmail.com>
2024-11-25 23:50:09 +0100

    Add button to open new window

--- classes.py
+++ classes.py
@@ -13,6 +13,22 @@ class App(customtkinter.CTk):
         self.frame_1 = Frame(self, name="Add Element", height=400)
         self.frame_1.grid(row=0, column=0, padx=20, pady=20, sticky="nsew")
 
+        # Category window
+        self.toplevel_window = None
+        # Button to open window
+        self.add_category_button = customtkinter.CTkButton(
+            self, 
+            text="Add Category",
+            command=self.add_category()
+            ) 
+        self.add_category_button.grid(row=1, column=0, padx=20, pady=5)
+
+    def add_category(self):
+        if self.toplevel_window is None or not self.toplevel_window.winfo_exists():
+            # Create window if it does not exists
+            self.toplevel_window = ToplevelWindow(self)
+        else:
+            self.toplevel_window.focus()
 
 
 
@@ -23,3 +39,15 @@ class Frame(customtkinter.CTkFrame):
         # Add label to frame
         self.label = customtkinter.CTkLabel(self, text=name)
         self.label.grid(row=0, column=0, padx=10)
+
+
+
+class ToplevelWindow(customtkinter.CTkToplevel):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.geometry("400x300")
+
+        self.label = customtkinter.CTkLabel(self, text="Add Category")
+        self.label.pack(padx=20, pady=20)
+
