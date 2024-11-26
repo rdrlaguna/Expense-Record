7aa1f468004a192f5d1ee549b957d332abd40acd
rdrlaguna <rdr.laguna@gmail.com>
2024-11-26 02:07:01 +0100

    Create categories table

--- backend.py
+++ backend.py
@@ -13,4 +13,24 @@ def database_connect():
         return None
     
 
-    
+def create_categories_table():
+    """ Create a categories table in database """
+
+    try:
+        # Connect to database
+        conn = database_connect()
+        cursor = conn.cursor()
+        # Execute query
+        cursor.execute('''
+            CREATE TABLE IF NOT EXISTS categories (
+                id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+                category_name TEXT NOT NULL
+            )
+        ''')
+        conn.commit()
+        cursor.close()
+    except sqlite3.Error as err:
+        print(f"Error creating notes table: {err}")
+
+
+        
