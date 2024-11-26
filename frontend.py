3b5c37beae514d40cbebe5ff0126a562fba251f0
rdrlaguna <rdr.laguna@gmail.com>
2024-11-26 01:28:33 +0100

    Add entry field to add new categories

--- frontend.py
+++ frontend.py
@@ -21,6 +21,7 @@ class App(customtkinter.CTk):
         self.categories_window = None
 
     def open_categories_window(self):
+        """ Open window to edit categories """
         if self.categories_window is None or not self.categories_window.winfo_exists():
             # Create window if its None or destroyed
             self.categories_window = CategoryWindow(self, name="CATEGORIES")  
@@ -32,6 +33,8 @@ class App(customtkinter.CTk):
 
 
 class Frame(customtkinter.CTkFrame):
+    """ Frame object with a label """
+
     def __init__(self, master, name="Add Name", **kwargs):
         super().__init__(master, **kwargs)
 
@@ -40,13 +43,34 @@ class Frame(customtkinter.CTkFrame):
         self.label.grid(row=0, column=0, padx=10)
 
 
+class EntryFrame(Frame):
+    """ Frame object with data entry option """
+
+    def __init__(self, master, name="Add Name", entry="Write here...", button="Submit", **kwargs):
+        super().__init__(master, name, **kwargs)
+
+        # Add entry field
+        self.entry_field = customtkinter.CTkEntry(self, placeholder_text=entry)
+        self.entry_field.grid(row=0, column=0, padx=20, pady=20, sticky="ew")
+
+        # Add button
+        self.button =customtkinter.CTkButton(self, text=button)
+        self.button.grid(row=2, column=0, padx=20, pady=5)
+
 
 class CategoryWindow(customtkinter.CTkToplevel):
+    """ Window to edit categories """
+
     def __init__(self, master, name="Title", *args, **kwargs):
         super().__init__(master, *args, **kwargs)
+
+        # Set up category window
         self.geometry("400x300")
         self.title(name)
-
-        self.label = customtkinter.CTkLabel(self, text=name)
-        self.label.pack(padx=20, pady=20)
-
+        # Add frame to add new category
+        self.category_add = EntryFrame(self, name="New Category", entry="Add new category...", button="Add")
+        self.category_add.grid(row=0, column=0, padx=20, pady=10, sticky="nsew")
+        
+        # TODO: 
+        # Show categories
+        # Edit category
