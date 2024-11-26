6d5ef548c39a3f2d2665afb752e4465efb48a8a8
rdrlaguna <rdr.laguna@gmail.com>
2024-11-26 01:52:33 +0100

    Create function to connect to database

--- backend.py
+++ backend.py
@@ -1,2 +1,16 @@
 import sqlite3
 from datetime import datetime
+
+
+def database_connect():
+    """ Connect to the database """
+
+    try:
+        conn = sqlite3.connect('expenses.db')
+        return conn
+    except sqlite3.Error as err:
+        print(f"Error connecting to the database: {err}")
+        return None
+    
+
+    
