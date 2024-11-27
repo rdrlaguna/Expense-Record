883a8ed176aa085f31b8cfe144326289141014c4
rdrlaguna <rdr.laguna@gmail.com>
2024-11-27 07:07:26 +0100

    Create function to validate category names

--- utils/validators.py
+++ utils/validators.py
@@ -1,1 +1,19 @@
 # utils/validators.py
+
+def validate_category_name(name):
+    """ Validates a category name. """
+
+    # Treat string case insensitive
+    # Validate string is not empty
+    if not name.strip():
+        raise ValueError(f"Category cannot be empty.")
+    
+    # Validate string has only letters
+    if not name.isalpha():
+        raise ValueError(f"The name must have only letters.")
+    
+    # Validate name lenght
+    if len(name) < 4 or len(name) > 16:
+        raise ValueError(f"Name must be between 4 to 16 characters long.")
+    
+    return True
